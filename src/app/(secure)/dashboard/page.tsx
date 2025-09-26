ea5160f160c7aaead042f69a1ced606ef78c6e9a
johngaynor <john.gaynor@irvmat.com>
2025-09-26 19:00:43 -0400

    setting up state management

--- src/app/(secure)/dashboard/page.tsx
+++ src/app/(secure)/dashboard/page.tsx
@@ -1,12 +1,10 @@
 "use client";
-import React from "react";
-import Dashboard from "./index";
 import PageTemplate from "../components/Templates/PageTemplate";
 
-export default function Page() {
+export default function DashboardPage() {
   return (
     <PageTemplate title="Today" showTitleMobile>
-      <Dashboard />
+      Dashboard
     </PageTemplate>
   );
 }
